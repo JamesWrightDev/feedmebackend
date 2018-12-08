2bbf2616141c0ca017e7b6716f6356a0be444d68
JamesWrightDev <32226552+JamesWrightDev@users.noreply.github.com>
2018-12-08 13:07:34 +0000

    inital commit

---
+++ app/Http/Controllers/FeedController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Feed;
+use Feeds;
+use Illuminate\Http\Request;
+
+class FeedController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      $u_feed = Feed::where('id', 1)->get();
+      $feed = Feeds::make('http://www.wsj.com/xml/rss/3_7085.xml');
+      $data = array(
+        'title'     => $feed->get_title(),
+        'permalink' => $feed->get_permalink(),
+        'items'     => $feed->get_items(),
+      );
+      $u=array();
+      foreach($data['items'] as $items){
+        array_push($u, $items->get_description());
+      }
+      dd($u);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Feed  $feed
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Feed $feed)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Feed  $feed
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Feed $feed)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Feed  $feed
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Feed $feed)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Feed  $feed
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Feed $feed)
+    {
+        //
+    }
+}
