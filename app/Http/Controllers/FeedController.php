4ccd0c346810ded8a8732fcbb6efdadeca6ff32e
James <jameswrightfilm@gmail.com>
2018-12-10 16:26:02 +0000

    update

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -14,14 +14,17 @@ class FeedController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
     public function index()
     {
         
         // Access User RSS Feeds
       $user_feed = Feed::all();
       $user_feed_url = array();
+      $user_feed_name = array();
       foreach($user_feed as $feed){
           array_push($user_feed_url, $feed->feed_url);
+          array_push($user_feed_name, $feed->feed_name);
       }  
       
     //   RSS Parser
@@ -43,10 +46,10 @@ class FeedController extends Controller
         }
       
 
-
-    
+        $response = array();
+        array_push($response, $data = array('articles'=>$a,'feeds' => $user_feed_name ));
     //   Return as JSON with 200 code. 
-      return response($a, 200);
+      return response($response, 200);
     }
 
     /**
@@ -68,15 +71,55 @@ class FeedController extends Controller
     public function store(Request $request)
     {
         // Veryify url
+        function verify_xml($url){
+            libxml_clear_errors();
+            libxml_use_internal_errors(true);
+            $xml = simplexml_load_file($url, 'SimpleXMLElement', LIBXML_NOCDATA );
+            if ($xml === false) {
+                dd('NOPE');
+                exit;                
+                } 
+            }
+                // $xml = simplexml_load_file($url, 'SimpleXMLElement', LIBXML_NOCDATA);
+                // if ($xml === false) {
+                //     throw new Exception("Cannot load xml source.\n");
+                //   }
+                // $a = array();
+                // foreach($xml->channel->item as $item)
+                //     {
+                //         array_push($a, $articles = array(
+                //             'outlet'=> (string)$xml->channel->title,
+                //             'title' => (string)$item->title,
+                //             'description' => (string)$item->description,
+                //             'date' => (string)$item->pubDate,
+                //             'link' => (string)$item->link,
+                //             ));
+                        
+                //     }
+            // }
+            
+            verify_xml('https://ww.wired.com/feed/rss');
+            
+            // try {
+            //     $var = $request['feed_url'];
+            //     verify_xml('https://www.wired.com/feed/rss');
+            // } catch (Exception $e) {
+            //     echo 'Caught exception: ',  $e->getMessage(), "\n";
+            // }
+
+            
+            
+            
+            
 
         // Create new feed entry in DB. 
 
-        $feed = new Feed;
-        $feed->feed_name = $request['name'];
-        $feed->feed_url = $request['feed_url'];
-        $feed->save();
+        // $feed = new Feed;
+        // $feed->feed_name = $request['name'];
+        // $feed->feed_url = $request['feed_url'];
+        // $feed->save();
         
-        return('Suc');
+        return('hello');
 
 
     }
