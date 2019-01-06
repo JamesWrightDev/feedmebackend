ac5a90bddd0db79d84c5db6ba3bccbdf37125292
James <contact@james-wright.co.uk>
2019-01-06 14:31:30 +0000

    switch to SimplePie

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -15,41 +15,70 @@ class FeedController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    public function index()
+    public function index(Request $request)
     {
-        
+        $user_feed = Feed::all();
+        $user_feed_url = array();
+        $user_feed_name = array();
+            foreach($user_feed as $feed){
+                array_push($user_feed_url, $feed->feed_url);
+                array_push($user_feed_name, $feed->feed_name);
+            }  
+
+
+        // Simple Pie
+        $results= array();
+        $feed = Feeds::make($user_feed_url);
+        $data = array(
+            'title'     => $feed->get_title(),
+            'permalink' => $feed->get_permalink(),
+            'items'     => $feed->get_items(),
+          );
+          foreach($data['items'] as $i){
+              array_push($results, $articles = array(
+                  
+                  'title' => $i->get_title(),
+                  'description' => $i->get_description(),
+                  'link' => $i->get_link(),
+                  'date' => $i->get_date(),
+
+              ));
+          }
+          return response($results);
+
+
         // Access User RSS Feeds
-      $user_feed = Feed::all();
-      $user_feed_url = array();
-      $user_feed_name = array();
-      foreach($user_feed as $feed){
-          array_push($user_feed_url, $feed->feed_url);
-          array_push($user_feed_name, $feed->feed_name);
-      }  
+    //   $user_feed = Feed::all();
+    //   $user_feed_url = array();
+    //   $user_feed_name = array();
+    //   foreach($user_feed as $feed){
+    //       array_push($user_feed_url, $feed->feed_url);
+    //       array_push($user_feed_name, $feed->feed_name);
+    //   }  
       
-    //   RSS Parser
-    $url = $user_feed_url;
-    $a = array();
-    foreach($user_feed_url as $url)
-        {
-            $xml = simplexml_load_file($url,'SimpleXMLElement', LIBXML_NOCDATA);
-            foreach($xml->channel->item as $item)
-                {
-                    array_push($a, $articles = array(
-                        'outlet'=> (string)$xml->channel->title,
-                        'title' => (string)$item->title,
-                        'description' => (string)$item->description,
-                        'date' => (string)$item->pubDate,
-                        'link' => (string)$item->link,
-                        ));
-                }
-        }
+    // //   RSS Parser
+    // $url = $user_feed_url;
+    // $a = array();
+    // foreach($user_feed_url as $url)
+    //     {
+    //         $xml = simplexml_load_file($url,'SimpleXMLElement', LIBXML_NOCDATA);
+    //         foreach($xml->channel->item as $item)
+    //             {
+    //                 array_push($a, $articles = array(
+    //                     'outlet'=> (string)$xml->channel->title,
+    //                     'title' => (string)$item->title,
+    //                     'description' => (string)$item->description,
+    //                     'date' => (string)$item->pubDate,
+    //                     'link' => (string)$item->link,
+    //                     ));
+    //             }
+    //     }
       
 
-        $response = array();
-        array_push($response, $data = array('articles'=>$a,'feeds' => $user_feed_name ));
-    //   Return as JSON with 200 code. 
-      return response($response, 200);
+    //     $response = array();
+    //     array_push($response, $data = array('articles'=> $a,'feeds' => $user_feed_name ));
+    // //   Return as JSON with 200 code. 
+      
     }
 
     /**
@@ -70,56 +99,34 @@ class FeedController extends Controller
      */
     public function store(Request $request)
     {
-        // Veryify url
-        function verify_xml($url){
-            libxml_clear_errors();
-            libxml_use_internal_errors(true);
-            $xml = simplexml_load_file($url, 'SimpleXMLElement', LIBXML_NOCDATA );
-            if ($xml === false) {
-                dd('NOPE');
-                exit;                
+        // Fetch URL
+            $feed_url = $request->feed_url;
+            $feed_name = $request->feed_name;
+            
+            $cSession = curl_init(); 
+            
+            curl_setopt($cSession,CURLOPT_URL,$feed_url);
+            curl_setopt($cSession,CURLOPT_RETURNTRANSFER,true);
+            curl_setopt($cSession,CURLOPT_HEADER, false); 
+            $result = curl_exec($cSession);
+            
+                // Check URL is valid
+                if (curl_error($cSession)) {
+                    $error_msg = curl_error($cSession);
+                    abort($error_msg);
+                }
+                // Check file recieved is XML. 
+                if(substr($result, 0, 5) !== "<?xml") {
+                    abort(403,'Not valid RSS'); 
                 } 
-            }
-                // $xml = simplexml_load_file($url, 'SimpleXMLElement', LIBXML_NOCDATA);
-                // if ($xml === false) {
-                //     throw new Exception("Cannot load xml source.\n");
-                //   }
-                // $a = array();
-                // foreach($xml->channel->item as $item)
-                //     {
-                //         array_push($a, $articles = array(
-                //             'outlet'=> (string)$xml->channel->title,
-                //             'title' => (string)$item->title,
-                //             'description' => (string)$item->description,
-                //             'date' => (string)$item->pubDate,
-                //             'link' => (string)$item->link,
-                //             ));
-                        
-                //     }
-            // }
+            $xml = simplexml_load_file($feed_url,'SimpleXMLElement', LIBXML_NOCDATA);
             
-            verify_xml('https://ww.wired.com/feed/rss');
-            
-            // try {
-            //     $var = $request['feed_url'];
-            //     verify_xml('https://www.wired.com/feed/rss');
-            // } catch (Exception $e) {
-            //     echo 'Caught exception: ',  $e->getMessage(), "\n";
-            // }
-
-            
-            
-            
-            
-
-        // Create new feed entry in DB. 
-
-        // $feed = new Feed;
-        // $feed->feed_name = $request['name'];
-        // $feed->feed_url = $request['feed_url'];
-        // $feed->save();
-        
-        return('hello');
+            curl_close($cSession);
+                $feed = new Feed;
+                $feed->feed_name = $xml->channel->title;
+                $feed->feed_url = $feed_url;
+                $feed->save();
+            return('Feed added');
 
 
     }
